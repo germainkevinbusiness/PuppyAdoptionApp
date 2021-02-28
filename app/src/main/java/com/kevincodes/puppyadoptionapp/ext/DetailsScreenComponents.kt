1ca3359ee9e2fdf2f8387512c394932648c8e36b
kevingermainbusiness <kevingermainbusiness@gmail.com>
2021-02-28 00:52:59 -0500

    Toast appears when tapping on "Adopt now !"

--- app/src/main/java/com/kevincodes/puppyadoptionapp/ext/DetailsScreenComponents.kt
+++ app/src/main/java/com/kevincodes/puppyadoptionapp/ext/DetailsScreenComponents.kt
@@ -1,5 +1,7 @@
 package com.kevincodes.puppyadoptionapp.ext
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -11,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,7 +84,25 @@ fun DetailsScreen(puppyId: Int?) {
                                     .fillMaxWidth()
                                     .padding(top = 8.dp)
                             ) {
-                                Button(onClick = { }) {
+                                Text(
+                                    text = "${puppy.adoptionPrice}USD",
+                                    color = Color.Black,
+                                    style = typography.body1
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 8.dp)
+                            ) {
+                                val context: Context = LocalContext.current
+                                Button(onClick = {
+                                    Toast.makeText(
+                                        context,
+                                        "\uD83C\uDF89 Congratulations, you successfully adopted ${puppy.name} !!! \uD83C\uDF89",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }) {
                                     Text(text = "Adopt now !")
                                 }
                             }
--- app/src/main/java/com/kevincodes/puppyadoptionapp/ext/HomeScreenComponents.kt
+++ app/src/main/java/com/kevincodes/puppyadoptionapp/ext/HomeScreenComponents.kt
@@ -5,12 +5,15 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,8 +44,11 @@ fun Header(shouldBackButtonBeVisible: Boolean, title: String, navController: Nav
                 painter = painterResource(
                     id = R.drawable.ic_baseline_arrow_back_ios_24,
                 ),
-                contentDescription = null,
-                modifier = Modifier.clickable { navController.popBackStack() }
+                contentDescription = "Go back button",
+                modifier = Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(bounded = true)
+                ) { navController.popBackStack() }
             )
         }
         Image(

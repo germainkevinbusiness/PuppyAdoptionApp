3ef6ecc90303c3b6ee5326c8c69179ad2c1e566c
germainkevinbusiness <67168053+kevingermainbusiness@users.noreply.github.com>
2021-10-07 23:24:47 -0400

    A lil Code clean up

--- app/src/main/java/com/kevincodes/puppyadoptionapp/PuppyAdoptionGraph.kt
+++ app/src/main/java/com/kevincodes/puppyadoptionapp/PuppyAdoptionGraph.kt
@@ -1,24 +1,17 @@
 package com.kevincodes.puppyadoptionapp
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.ExperimentalMaterialApi
+
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.kevincodes.puppyadoptionapp.data.PuppiesRepo
-import com.kevincodes.puppyadoptionapp.data.Puppy
 import com.kevincodes.puppyadoptionapp.ui.details.DetailScreen
 import com.kevincodes.puppyadoptionapp.ui.home.HomeScreen
 import com.kevincodes.puppyadoptionapp.utils.Utils
-import kotlinx.coroutines.launch
 
 //Navigation graph destinations
 object MainDestinations {
@@ -43,13 +36,12 @@ fun PuppyAdoptionGraph(mainActivity: MainActivity) {
     val mainNavigationActions = remember(navController) {
         MainNavigationActions(navController)
     }
-    val systemUiController = rememberSystemUiController()
     val utils by lazy { Utils }
 
-//    SideEffect {
-//        systemUiController.statusBarDarkContentEnabled = utils.isNightModeTheme(mainActivity)
+//    val puppies = produceState(initialValue = emptyList<Puppy>()) {
+//        PuppiesRepo.getPuppies()
 //    }
-    val puppies = PuppiesRepo.getPuppies()
+    val puppies by lazy { PuppiesRepo.getPuppies() }
     Scaffold(
         scaffoldState = scaffoldState
     ) {
